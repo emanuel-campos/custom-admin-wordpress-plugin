311dea1e9b0c5ae764693a1eed96c683762d82a9
Emanuel Campos <emanuelcampos.contato@gmail.com>
2017-11-08 13:57:08 -0300

    base do plugin

---
+++ includes/class-custom-admin-activator.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @since      1.0.0
+ *
+ * @package    CustomAdmin
+ * @subpackage CustomAdmin/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    CustomAdmin
+ * @subpackage CustomAdmin/includes
+ * @author     Emanuel Campos <emanuelcampos.contato@gmail.com>
+ */
+class Custom_Admin_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
