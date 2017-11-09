2ab833f69529677a1bf23b32b11a291aa16cae90
Emanuel Campos <emanuelcampos.contato@gmail.com>
2017-11-09 13:32:02 -0300

    criar base do formulario para a pagina de configuracoes do plugin

--- admin/partials/plugin-custom-admin-display.php
+++ admin/partials/plugin-custom-admin-display.php
@@ -15,4 +15,19 @@
 <!-- This file should primarily consist of HTML with a little bit of PHP. -->
 <div class="wrap">
 	<h1 class="wp-heading-inline">Custom Admin</h1>
+	<form>
+		<table class="form-table">
+			<tbody>
+				<tr>
+					<th scope="row"><label for="lorem">Lorem Ipsum</label></th>
+					<td><input name="lorem" id="lorem" value="" class="regular-text" type="text"></td>
+				</tr>
+				<tr>
+					<th scope="row"><label for="aenean_dictum">Aenean dictum</label></th>
+					<td><input name="aenean_dictum" id="aenean_dictum" aria-describedby="tagline-description" value="" class="regular-text" type="text">
+					<p class="description">Aenean dictum, nulla ultrices mattis rhoncus, massa turpis.</p></td>
+				</tr>
+			</tbody>
+		</table>
+	</form>
 </div>
